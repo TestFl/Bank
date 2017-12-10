faeed8edab4ccdb044ee3f1fae7f707ef0f723a1
Flissek <flissek@gmail.com>
2017-12-10 21:30:22 +0100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+public class Client {
+    String firstName;
+    String lastName;
+    int phoneNumber;
+    long accountNumber;
+
+    public Client(String firstName, String lastName, int phoneNumber, long accountNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.accountNumber = accountNumber;
+    }
+
+}
