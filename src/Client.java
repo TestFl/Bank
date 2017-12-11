e87a8a6944574cff46e7bb6d289884c9080ed95f
Flissek <flissek@gmail.com>
2017-12-11 20:10:56 +0100

    client features

--- src/Client.java
+++ src/Client.java
@@ -1,14 +1,34 @@
-public class Client {
-    String firstName;
-    String lastName;
-    int phoneNumber;
-    long accountNumber;
+import java.util.ArrayList;
 
-    public Client(String firstName, String lastName, int phoneNumber, long accountNumber) {
+public class Client {
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private int bankPersonalNumber;
+    private ArrayList<Double> transactions;
+
+
+
+    public Client(String firstName, String lastName, int phoneNumber, long bankPersonalNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
-        this.accountNumber = accountNumber;
+        this.bankPersonalNumber = bankPersonalNumber;
     }
+
+    public void addTransaction(double amount){
+        this.transactions.add(amount);
+    }
+     public int crateAccounNumber(){
+        int newAccountNumber = 10000 +(int) Math.random()* 90000;
+        if (getAccountNumber() == newAccountNumber){
+            int newAccountNumber = 10000 +(int) Math.random()* 90000;
+        }
+        return newAccountNumber;
+     }
+
+     private int getAccountNumber(){
+         return 0;
+     }
 
 }
